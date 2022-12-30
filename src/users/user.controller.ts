5a03305556dfa34cd4b1e7297aad5e359c83e237
Andrey <Hitmanki@yandex.ru>
2022-12-30 09:46:28 +0400

    add profile endpoint and acept image upload

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,18 +1,41 @@
-import { Controller, Get, HttpException, HttpStatus, Inject, Query } from "@nestjs/common";
+import {
+	Body,
+	Controller,
+	Get,
+	HttpException,
+	HttpStatus,
+	Inject,
+	Post,
+	Query,
+	UploadedFile,
+	UseInterceptors,
+} from "@nestjs/common";
 import { Routes, Services } from "src/utils/constants";
 import { IUserService } from "./user";
+import { FileInterceptor } from "@nestjs/platform-express/multer";
+import { UserProfileDto } from "./dtos/UserProfile";
 
 @Controller(Routes.USERS)
 export class UsersController {
 	constructor(
 		@Inject(Services.USERS) private readonly userService: IUserService,
-	) {}
-	
+	) { }
+
 	@Get('search')
 	searchUsers(@Query('query') query: string) {
 		console.log(query);
 		if (!query)
 			throw new HttpException('Provide a valid query', HttpStatus.BAD_REQUEST);
 		return this.userService.searchUsers(query);
+	}
+
+	@Post('profiles')
+	@UseInterceptors(FileInterceptor('file'))
+	async completeProfile(
+		@UploadedFile() file: Express.Multer.File,
+		@Body() userProfileDto: UserProfileDto,
+	) {
+		console.log(file);
+		console.log(userProfileDto.about, userProfileDto.username);
 	}
 }
